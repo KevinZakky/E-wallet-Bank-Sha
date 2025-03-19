7abce1849ee4aeafead71b4aa9113fde02eb9f62
KevinZakky <kevinzakky9@gmail.com>
2025-03-19 14:54:31 +0700

    feat: added floating action button

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -8,43 +8,63 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: lightBackgroundColor,
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: blueColor,
-        unselectedItemColor: blackColor,
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
-        selectedLabelStyle:
-            blueTextStyle.copyWith(fontSize: 10, fontWeight: medium),
-        unselectedLabelStyle:
-            blackTextStyle.copyWith(fontSize: 10, fontWeight: medium),
-        items: [
-          BottomNavigationBarItem(
-              icon: Image.asset(
-                'assets/ic_overview.png',
-                width: 20,
-                color: blueColor,
-              ),
-              label: 'Overview'),
-          BottomNavigationBarItem(
-              icon: Image.asset(
-                'assets/ic_history.png',
-                width: 20,
-              ),
-              label: 'History'),
-          BottomNavigationBarItem(
-              icon: Image.asset(
-                'assets/ic_statistic.png',
-                width: 20,
-              ),
-              label: 'Statistic'),
-          BottomNavigationBarItem(
-              icon: Image.asset(
-                'assets/ic_reward.png',
-                width: 20,
-              ),
-              label: 'Reward'),
-        ],
+      bottomNavigationBar: BottomAppBar(
+        elevation: 0,
+        color: whiteColor,
+        shape: CircularNotchedRectangle(),
+        clipBehavior: Clip.antiAlias,
+        notchMargin: 6,
+        child: BottomNavigationBar(
+          elevation: 0,
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: whiteColor,
+          selectedItemColor: blueColor,
+          unselectedItemColor: blackColor,
+          showSelectedLabels: true,
+          showUnselectedLabels: true,
+          selectedLabelStyle:
+              blueTextStyle.copyWith(fontSize: 10, fontWeight: medium),
+          unselectedLabelStyle:
+              blackTextStyle.copyWith(fontSize: 10, fontWeight: medium),
+          items: [
+            BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/ic_overview.png',
+                  width: 20,
+                  color: blueColor,
+                ),
+                label: 'Overview'),
+            BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/ic_history.png',
+                  width: 20,
+                ),
+                label: 'History'),
+            BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/ic_statistic.png',
+                  width: 20,
+                ),
+                label: 'Statistic'),
+            BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/ic_reward.png',
+                  width: 20,
+                ),
+                label: 'Reward'),
+          ],
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        shape: CircleBorder(),
+        backgroundColor: purpleColor,
+        child: Image.asset(
+          'assets/ic_plus_circle.png',
+          width: 24,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
