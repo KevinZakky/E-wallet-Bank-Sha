ce8c382b7e5f4cb30b32838235177eabb181d9a6
KevinZakky <kevinzakky9@gmail.com>
2025-03-26 16:00:33 +0700

    feat: slicing edit page

--- lib/ui/pages/profile_edit_page.dart
+++ lib/ui/pages/profile_edit_page.dart
@@ -1,3 +1,6 @@
+import 'package:ewallet_project_app/shared/theme.dart';
+import 'package:ewallet_project_app/ui/widgets/Forms.dart';
+import 'package:ewallet_project_app/ui/widgets/buttons.dart';
 import 'package:flutter/material.dart';
 
 class ProfileEditPage extends StatelessWidget {
@@ -5,6 +8,54 @@ class ProfileEditPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Profile'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          Container(
+            padding: const EdgeInsets.all(22),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20), color: whiteColor),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CustomFormField(title: 'Username'),
+                const SizedBox(
+                  height: 16,
+                ),
+                CustomFormField(title: 'Full Name'),
+                const SizedBox(
+                  height: 16,
+                ),
+                CustomFormField(title: 'Email Address'),
+                const SizedBox(
+                  height: 16,
+                ),
+                CustomFormField(
+                  title: 'Password',
+                  obscureText: true,
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                CustomFilledButton(
+                  title: 'Update Now',
+                  onPressed: () {},
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
