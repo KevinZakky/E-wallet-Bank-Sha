e22f5df8d8ce8e2523d0e2a37e3e11d6542d6c33
KevinZakky <kevinzakky9@gmail.com>
2025-04-09 14:44:25 +0700

    feat: slicing item within container at more dialog

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -446,9 +446,62 @@ class MoreDialog extends StatelessWidget {
       content: Container(
         width: MediaQuery.of(context).size.width,
         height: 326,
+        padding: const EdgeInsets.all(30),
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(40),
-            color: lightBackgroundColor),
+          borderRadius: BorderRadius.circular(40),
+          color: lightBackgroundColor,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Do More With Us',
+              style: blackTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: semiBold,
+              ),
+            ),
+            const SizedBox(
+              height: 13,
+            ),
+            Wrap(
+              spacing: 29,
+              runSpacing: 29,
+              children: [
+                HomeServices(
+                  iconUrl: 'assets/ic_product_data.png',
+                  title: 'Data',
+                  onTap: () {},
+                ),
+                HomeServices(
+                  iconUrl: 'assets/ic_product_water.png',
+                  title: 'Water',
+                  onTap: () {},
+                ),
+                HomeServices(
+                  iconUrl: 'assets/ic_product_stream.png',
+                  title: 'Stream',
+                  onTap: () {},
+                ),
+                HomeServices(
+                  iconUrl: 'assets/ic_product_movie.png',
+                  title: 'Movie',
+                  onTap: () {},
+                ),
+                HomeServices(
+                  iconUrl: 'assets/ic_product_food.png',
+                  title: 'Food',
+                  onTap: () {},
+                ),
+                HomeServices(
+                  iconUrl: 'assets/ic_product_travel.png',
+                  title: 'Travel',
+                  onTap: () {},
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
